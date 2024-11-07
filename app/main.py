716b5ede51ab324b08e602bda388121d390d44f7
Jackson Goode <jacksongoode@proton.me>
2024-11-07 10:42:14 -0800

    Oops Etag

--- app/main.py
+++ app/main.py
@@ -133,7 +133,7 @@ def get_svg():
     if track_data and track_data["svg"]:
         response = Response(track_data["svg"], mimetype="image/svg+xml")
         response.headers["Cache-Control"] = "public, max-age=60, s-maxage=60, must-revalidate"
-        response.headers["X-Version"] = VERCEL_COMMIT_SHA
+        response.headers["ETag"] = f'W/"{VERCEL_COMMIT_SHA}"'
         logger.info(f"Served current track SVG: {track_data['song']} by {track_data['artist']}")
         return response
     logger.error("Current track SVG not ready")
@@ -180,7 +180,7 @@ def daylist():
         
         response = Response(svg, mimetype="image/svg+xml")
         response.headers["Cache-Control"] = "public, max-age=1800, s-maxage=1800, must-revalidate"
-        response.headers["X-Version"] = VERCEL_COMMIT_SHA
+        response.headers["ETag"] = f'W/"{VERCEL_COMMIT_SHA}"'
         print(f"INFO: Served daylist SVG: {daylist_phrase}")  # Direct stdout print
         return response
         
